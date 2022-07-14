da297c3ab167fd39d20183e07de949d1a2d1ee4b
DaCodeWiz <jake.pearson.1694@gmail.com>
2022-07-14 00:05:50 -0700

    new portfolio site

---
+++ index.js
@@ -0,0 +1,13 @@
+// template_gq9r84e
+// service_q9h7skv
+//Vuqk4dWzU6PLP1Qw6 public key
+
+function contact() {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    settimeout(() =>{
+        console.log('it worked lol')
+    }, 500);
+}
