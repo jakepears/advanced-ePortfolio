c6d0f57d8cf5879b64bd5064d1c5d2110c475cef
DaCodeWiz <jake.pearson.1694@gmail.com>
2022-07-15 20:15:57 -0700

    complete :D
    it's done, just final touches now

--- index.js
+++ index.js
@@ -3,6 +3,30 @@
 //Vuqk4dWzU6PLP1Qw6 public key
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 25;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+  
+    for (let i = 0; i < shapes.length; ++i) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      // Added rotate after tutorial
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 8}deg)`
+    }
+  }
+  
+  function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+      document.body.classList += " dark-theme"
+    }
+    else {
+      document.body.classList.remove("dark-theme")
+    }
+  }
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
