a01585a4d5b1dfc27b7411d1c1305332aecfce68
DaCodeWiz <jake.pearson.1694@gmail.com>
2022-09-07 18:58:57 -0700

    new changes, not working

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// template_gq9r84e
-// service_q9h7skv
-//Vuqk4dWzU6PLP1Qw6 public key
 let isModalOpen = false;
 let contrastToggle = false;
 const scaleFactor = 1 / 25;
@@ -13,7 +10,6 @@ function moveBackground(event) {
     for (let i = 0; i < shapes.length; ++i) {
       const isOdd = i % 2 !== 0;
       const boolInt = isOdd ? -1 : 1;
-      // Added rotate after tutorial
       shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 8}deg)`
     }
   }
@@ -71,10 +67,28 @@ function toggleModal() {
     isModalOpen = true;
     // toggle modal
     document.body.classList += " modal--open";
-
-
 }
 
-// gsap animation 
-gsap.to(projects, {duration: 2, scrollTo:"#row"});
  
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD9iLlyJ_1AgXB3sLRJ84ZUW-tcLNkKc_Y",
+  authDomain: "personal-portfolio-6f3f6.firebaseapp.com",
+  projectId: "personal-portfolio-6f3f6",
+  storageBucket: "personal-portfolio-6f3f6.appspot.com",
+  messagingSenderId: "799279348349",
+  appId: "1:799279348349:web:f252af094fa2d0b6074f14",
+  measurementId: "G-HTP64SKTG6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
